23a005d9db3de573054e75704cd29a687d4a9eb6
Data Chani <dchani@ox-s1.msk.21-school.ru>
2021-01-08 14:16:14 +0300

    second commit

--- srcs/script.sh
+++ srcs/script.sh
@@ -3,11 +3,13 @@
 service php7.3-fpm start
 
 service mysql start
+{
+echo "CREATE USER 'dchani'@'localhost';"
+echo "CREATE DATABASE IF NOT EXISTS wordpress;"
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'dchani'@'localhost';"
+echo "FLUSH PRIVILEGES;"
+} | mysql -u root --skip-password
 
-echo "CREATE DATABASE IF NOT EXISTS wordpress;" | mysql -u root --skip-password
-echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost';" | mysql -u root --skip-password
-echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
-echo "update mysql.user set plugin = 'mysql_native_password' where user='root';" | mysql -u root
 service nginx start
 
 bash
